7817424c97683c4ab9f73be217466fe7b9c39dc8
johnymitchel <34136120+johnymitchel@users.noreply.github.com>
2017-11-30 16:22:12 +0000

    Update hello.go

--- hello/hello.go
+++ hello/hello.go
@@ -5,7 +5,7 @@ Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
 
-     http://www.apache.org/licenses/LICENSE-2.0
+     http://www.apache.org/licenses/LICENSE-2.0 TEST
 
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
@@ -23,5 +23,5 @@ import (
 )
 
 func main() {
-	fmt.Println(stringutil.Reverse("!selpmaxe oG ,olleH"))
+	fmt.Println(stringutil.Reverse("!selpmaxe oGz ,olleH"))
 }
